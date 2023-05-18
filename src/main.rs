8a957ab8dd2072b4875aef3b88626d561d14f8f6
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2023-05-18 20:29:58 +0300

    feat: sort images to suitable for widescreen+ and unsuitable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate walkdir;
 use walkdir::WalkDir;
 use imagesize::size;
+use std::collections::{HashMap, HashSet};
 use std::path::Path;
 use std::ffi::OsStr;
 use std::fs;
@@ -14,6 +15,23 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
     .and_then(OsStr::to_str)
 }
 
+fn gcd (a:usize, b: usize) -> usize{
+    if b == 0 {
+        return a
+    }
+    return gcd (b, a % b)
+}
+
+fn gcd_cached (a: usize, b: usize, cache: &mut HashMap<String, usize>) -> usize {
+    let cache_key = format!("{}x{}", a, b);
+    if cache.contains_key(&cache_key) {
+        return cache[&cache_key];
+    }
+    let r = gcd(a,b);
+    cache.insert(cache_key, r);
+    return r;
+}
+
 fn main() {
     let mut total_landscape = 0;
     let mut total_portrait = 0;
@@ -23,9 +41,13 @@ fn main() {
     let mut total_images = 0;
     let mut total_files = 0;
     let mut total_bytes: u64 = 0;
+    let mut total_suitable = 0;
+    let mut total_unsuitable = 0;
     let mut source: String = "".to_owned();
     let mut target: String = "".to_owned();
     let mut out_dir: PathBuf = PathBuf::new();
+    let mut gcd_cache: HashMap<String, usize> = HashMap::new();
+    let mut aspect_ratios: HashSet<String> = HashSet::new();
     if args().count() < 2 {
         println!("Usage: nerja <SOURCE> [TARGET]
 
@@ -77,7 +99,19 @@ Options:
                 total_hd += 1;
                 if width > 0 && height > 0 {
                     if width > height {
+                        let mut widescreen_suitable = false;
+                        let width_ceil = (((width/100) as f64).round() as usize) * 100;
+                        let height_ceil = (((height/100) as f64).round() as usize) * 100;
                         total_landscape += 1;
+                        let r = gcd_cached(width_ceil, height_ceil, &mut gcd_cache);
+                        aspect_ratios.insert(format!("{}:{}",width_ceil/r,height_ceil/r));
+                        let aspect_ratio_decimal = (width_ceil / r) as f64 / (height_ceil/r) as f64;
+                        if aspect_ratio_decimal >= 1.6 && aspect_ratio_decimal <= 1.9 {
+                            total_suitable += 1;
+                            widescreen_suitable = true;
+                        } else {
+                            total_unsuitable += 1;
+                        }
                         if target.is_empty() {
                             // report only
                             continue
@@ -85,7 +119,14 @@ Options:
                         let from = file.path();
                         let path_to_copy = from.strip_prefix(&in_dir)
                             .expect("path is not part of the prefix");
-                        let to = out_dir.join(path_to_copy);
+                        let mut to = out_dir.clone();
+                        if widescreen_suitable {
+                            to = to.join("widescreen")
+                        } else {
+                            to = to.join("normal")
+                        }
+                        //let to = out_dir.join(path_to_copy);
+                        to = to.join(path_to_copy);
                         let to_dir = to.parent().expect("target path must be in some directory");
                         if !Path::new(to_dir).is_dir() {
                             fs::create_dir_all(to_dir).expect("destination path creation failed");
@@ -111,6 +152,7 @@ Options:
     }
     pb.finish_with_message("done");
     println!("TOTAL {} HD images, {} landscape and {} portrait", total_hd, total_landscape, total_portrait);
+    println!("WIDE SCREEN+ SUITABLE {} images and {} unsuitable images", total_suitable, total_unsuitable);
     println!("SKIPPED {}, COPIED {} HD landscape images", total_skipped, total_copied);
     println!("total of {} files, {} images, {} bytes", total_files, total_images, total_bytes);
 }
